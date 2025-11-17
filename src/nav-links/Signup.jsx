423d4158abe943f666993acd5a39c32c00e2f05e
vineeth <119692597+vineeth38@users.noreply.github.com>
2025-11-17 12:05:22 +0530

    Signup.jsx

--- src/nav-links/Signup.jsx
+++ src/nav-links/Signup.jsx
@@ -29,7 +29,7 @@ export const Signup = ()=>{
 
                try{
                 const response = await axios.post(
-                    "http://127.0.0.1:8000/Signup/",
+                    "https://hotel-stays-booking.onrender.com/Signup/",
                     formData,
                      { headers: { "Content-Type": "application/json" }}
                 );
